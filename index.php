d5f5f4af66011e92d94e9c528d75b5214bcddd7f
vitorguimars <vitorguima.rs@bol.com.br>
2016-09-26 22:19:13 -0300

    Realizado a criação do template de login e adicionado template base.

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X=UA-Compatible" content="IE-edge">
+<title>Login</title>
+<link rel="stylesheet" href="css/estilo.css">
+<link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<div class="container">
+  <div class="cabecalho">
+  </div>
+<form action="principal.php" method="post" accept-charset="utf-8" class="form-login">
+<h2 class="form-login-heading text-center">Acesso</h2>
+<label for="inputUsuario" class="sr-only">Usuário</label>
+<input type="usuario" id="inputUsuario" class="form-control" placeholder="Usuario" required autofocus>
+<label for="senha" class="sr-only">Senha</label>
+<input type="password" id="inputSenha" class="form-control" placeholder="Senha" required>
+<div class="checkbox">
+  <label>
+    <input type="checkbox" value="esqueci">Esquici a senha </label>
+  </div>
+  <button type="submit" class="btn btn-lg btn-primary btn-block">Entrar</button>
+<footer padding-top:60px>
+
+</footer>
+
+
+
+</body>
+</html>
